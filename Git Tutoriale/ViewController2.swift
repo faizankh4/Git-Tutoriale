66f64cbc301f5f2832d7b935f164a20bbaf3cf6b
Faizan Khan <faizankh4@gmail.com>
2019-02-19 17:37:11 +0530

    addvw2

---
+++ Git Tutoriale/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Git Tutoriale
+//
+//  Created by TechMadmin on 19/02/19.
+//  Copyright © 2019 TechMadmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
